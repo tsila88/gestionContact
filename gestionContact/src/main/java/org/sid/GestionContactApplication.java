7cd163ffbe5cba36218cc60cd1dfd3eccf4ea8e0
tsilavina Ratsimbazafy <aina-tsilavina.ratsimbazafy@fortil.fr>
2020-01-07 09:41:11 +0100

    First commit, add Back-end

---
+++ gestionContact/src/main/java/org/sid/GestionContactApplication.java
@@ -0,0 +1,13 @@
+package org.sid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionContactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionContactApplication.class, args);
+	}
+
+}
